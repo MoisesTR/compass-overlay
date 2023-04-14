66573c928c6474306b0464fb6786229a069a9e76
Moises-TR <80355543+Moises-TR@users.noreply.github.com>
2023-04-14 13:53:15 -0600

    add button to capture photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 
-const BackCamera: React.FC = () => {
+const Camera: React.FC = () => {
+  const [photoData, setPhotoData] = useState<string | null>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
     navigator.mediaDevices
@@ -16,11 +18,30 @@ const BackCamera: React.FC = () => {
       });
   }, []);
 
+  const takePhoto = () => {
+    if (videoRef.current && canvasRef.current) {
+      const context = canvasRef.current.getContext("2d");
+      if (context) {
+        const videoWidth = videoRef.current.videoWidth;
+        const videoHeight = videoRef.current.videoHeight;
+        canvasRef.current.width = videoWidth;
+        canvasRef.current.height = videoHeight;
+        context.drawImage(videoRef.current, 0, 0, videoWidth, videoHeight);
+        setPhotoData(canvasRef.current.toDataURL("image/png"));
+      }
+    }
+  };
+
   return (
     <div>
       <video ref={videoRef} autoPlay />
+      <canvas ref={canvasRef} style={{ display: "none" }} />
+      {photoData && (
+        <img src={photoData} alt="captured photo" style={{ maxWidth: "100%" }} />
+      )}
+      <button onClick={takePhoto}>Take Photo</button>
     </div>
   );
 };
 
-export default BackCamera;
+export default Camera;
