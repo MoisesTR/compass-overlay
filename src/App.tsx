8aa1f5ff314025bd3b05f53e73b865157e0b54d2
Moises-TR <80355543+Moises-TR@users.noreply.github.com>
2023-04-14 14:16:53 -0600

    add compass overlay

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ const Camera: React.FC = () => {
   const [photoData, setPhotoData] = useState<string | null>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const compassRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     navigator.mediaDevices
@@ -16,6 +17,16 @@ const Camera: React.FC = () => {
       .catch((error) => {
         console.error(error);
       });
+  }, []);
+
+  useEffect(() => {
+    const handleCompass = (event: DeviceOrientationEvent) => {
+      if (compassRef.current) {
+        compassRef.current.style.transform = `rotate(${-event.alpha}deg)`;
+      }
+    };
+    window.addEventListener("deviceorientation", handleCompass);
+    return () => window.removeEventListener("deviceorientation", handleCompass);
   }, []);
 
   const takePhoto = () => {
@@ -33,13 +44,31 @@ const Camera: React.FC = () => {
   };
 
   return (
-    <div>
+    <div style={{ position: "relative" }}>
       <video ref={videoRef} autoPlay />
       <canvas ref={canvasRef} style={{ display: "none" }} />
       {photoData && (
         <img src={photoData} alt="captured photo" style={{ maxWidth: "100%" }} />
       )}
       <button onClick={takePhoto}>Take Photo</button>
+      <div
+        ref={compassRef}
+        style={{
+          position: "absolute",
+          top: "10px",
+          left: "10px",
+          width: "50px",
+          height: "50px",
+          backgroundColor: "rgba(255, 255, 255, 0.5)",
+          borderRadius: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          boxShadow: "0px 0px 5px rgba(0, 0, 0, 0.5)",
+        }}
+      >
+        <i className="fas fa-compass" style={{ fontSize: "24px" }} />
+      </div>
     </div>
   );
 };
