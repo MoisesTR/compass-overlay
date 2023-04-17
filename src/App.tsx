8179972ca09bb8499c6a223f7589d2c71ed6d2f3
Moises-TR <80355543+Moises-TR@users.noreply.github.com>
2023-04-17 16:06:01 -0600

    arrow heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useRef, useEffect, useState } from 'react';
 import CompassImage from './assets/compass.svg'; // Adjust the path if necessary
+import ArrowImage from './assets/arrow.svg'; // Adjust the path if necessary
 
 const CompassCamera: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -47,19 +48,40 @@ const CompassCamera: React.FC = () => {
         autoPlay
         playsInline
       />
-      <img
-        src={CompassImage}
-        alt="Compass"
+      <div
         style={{
           position: 'absolute',
-          bottom: 20,
+          top: '50%',
           left: '50%',
-          transform: `translateX(-50%) rotate(${360 - heading}deg)`,
-          width: 150,
-          height: 150,
-          zIndex: 2,
+          transform: 'translate(-50%, -50%)',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          color: 'white',
         }}
-      />
+      >
+        <div style={{ fontSize: '24px', marginBottom: '8px' }}>Heading: {Math.round(heading)}°</div>
+        <div style={{ width: '150px', height: '150px', position: 'relative' }}>
+          <img
+            src={CompassImage}
+            alt="Compass"
+            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2 }}
+          />
+          <img
+            src={ArrowImage}
+            alt="Arrow"
+            style={{
+              position: 'absolute',
+              top: '50%',
+              left: '50%',
+              transform: `translate(-50%, -50%) rotate(${heading}deg)`,
+              width: '60%',
+              height: '60%',
+              zIndex: 3,
+            }}
+          />
+        </div>
+      </div>
     </div>
   );
 };
