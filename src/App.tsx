d8742cae40c32ed7ce62edea416590af9b9caaa9
Moises-TR <80355543+Moises-TR@users.noreply.github.com>
2023-04-18 11:44:49 -0600

    add new compass image

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect, useState } from 'react';
-import CompassImage from './assets/compass_2.svg'; // Adjust the path if necessary
+import CompassImage from './assets/compass_3.svg'; // Adjust the path if necessary
 import ArrowImage from './assets/arrow.svg'; // Adjust the path if necessary
 
 const CompassCamera: React.FC = () => {
