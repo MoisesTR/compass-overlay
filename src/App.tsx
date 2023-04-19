1489f8180133708d1e170b0387b5f904d4042aa6
Moises-TR <80355543+Moises-TR@users.noreply.github.com>
2023-04-19 02:33:37 -0600

    rotate compass

--- src/App.tsx
+++ src/App.tsx
@@ -185,7 +185,7 @@ const CompassCamera: React.FC = () => {
           <img
             src={CompassImage}
             alt="Compass"
-            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2 }}
+            style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 2, transform: `translate(-50%, -50%) rotate(${Math.round(heading)}deg)` }}
           />
           <img
             src={ArrowImage}
