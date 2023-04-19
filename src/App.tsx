feda61ab7e110ae25c1f018f47e71b0bca7d5a08
Moises-TR <80355543+Moises-TR@users.noreply.github.com>
2023-04-19 01:57:37 -0600

    don't rotate location and heading text in capture

--- src/App.tsx
+++ src/App.tsx
@@ -38,21 +38,11 @@ const CompassCamera: React.FC = () => {
     canvas.width = videoRef.current?.videoWidth || 0;
     canvas.height = videoRef.current?.videoHeight || 0;
     const ctx = canvas.getContext('2d');
-
+  
     if (ctx) {
       // Draw the video frame
       ctx.drawImage(videoRef.current!, 0, 0, canvas.width, canvas.height);
-
-      // Get the current latitude and longitude
-      let latitude = 0;
-      let longitude = 0;
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-          latitude = position.coords.latitude;
-          longitude = position.coords.longitude;
-        });
-      }
-
+  
       // Draw the compass and arrow
       const compass = new Image();
       compass.src = CompassImage;
@@ -60,37 +50,30 @@ const CompassCamera: React.FC = () => {
       const compassX = (canvas.width - compassSize) / 2;
       const compassY = (canvas.height - compassSize) / 2;
       compass.onload = () => {
-        // Determine the angle to rotate the compass based on the current heading
-        const compassRotation = (360 - heading) % 360;
-
-        // Rotate the canvas to adjust for device orientation and arrow orientation
-        const orientation = window.screen.orientation?.angle || 0;
-        ctx.translate(canvas.width / 2, canvas.height / 2);
-        ctx.rotate(((compassRotation - orientation) * Math.PI) / 180);
-        ctx.translate(-canvas.width / 2, -canvas.height / 2);
-
         ctx.drawImage(compass, compassX, compassY, compassSize, compassSize);
-
+  
         const arrow = new Image();
         arrow.src = ArrowImage;
         const arrowSize = compassSize * 0.5;
         const arrowX = (canvas.width - arrowSize) / 2;
         const arrowY = (canvas.height - arrowSize) / 2;
         arrow.onload = () => {
-          // Rotate the arrow based on the heading angle
+          // Rotate the canvas to adjust for device orientation and arrow orientation
+          const orientation = window.screen.orientation?.angle || 0;
+          ctx.save(); // save the canvas transformation state
           ctx.translate(canvas.width / 2, canvas.height / 2);
-          ctx.rotate(((360 - heading + orientation - 90) * Math.PI) / 180);
+          ctx.rotate(((heading - orientation - 90) * Math.PI) / 180);
           ctx.translate(-canvas.width / 2, -canvas.height / 2);
-
+  
           ctx.drawImage(arrow, arrowX, arrowY, arrowSize, arrowSize);
-
-          // Draw the heading text and current location
+  
+          ctx.restore(); // restore the canvas transformation state
+  
+          // Draw the heading text
           const headingText = `Heading: ${Math.round(heading)}°`;
-          const locationText = `Latitude: ${latitude}, Longitude: ${longitude}`;
           const fontSize = 24;
           const textX = canvas.width / 2;
           const textY = (canvas.height - compassSize) / 2 - fontSize;
-          const locationY = (canvas.height + compassSize) / 2 + fontSize;
           ctx.font = `${fontSize}px sans-serif`;
           ctx.textAlign = 'center';
           ctx.fillStyle = 'white';
@@ -98,9 +81,13 @@ const CompassCamera: React.FC = () => {
           ctx.lineWidth = 4;
           ctx.strokeText(headingText, textX, textY);
           ctx.fillText(headingText, textX, textY);
+  
+          // Draw the location text
+          const locationText = `Lat: ${latitude?.toFixed(5)}, Lon: ${longitude?.toFixed(5)}`;
+          const locationY = (canvas.height + compassSize) / 2 + fontSize * 2;
           ctx.strokeText(locationText, textX, locationY);
           ctx.fillText(locationText, textX, locationY);
-
+  
           // Save the captured image
           const imageData = canvas.toDataURL('image/jpeg');
           const link = document.createElement('a');
@@ -111,8 +98,6 @@ const CompassCamera: React.FC = () => {
       };
     }
   };
-
-
 
   useEffect(() => {
     const startCamera = async () => {
